b142a6794ce4052063fe74a58828a66e136010de
QBitcoin Developer 1 <qbitcoin.dev@gmail.com>
2026-03-01 18:07:58 +0100

    Burn transactions decrease block->upgraded

--- lib/QBitcoin/ValueUpgraded.pm
+++ lib/QBitcoin/ValueUpgraded.pm
@@ -10,7 +10,7 @@ use QBitcoin::ORM qw(find create :types);
 use QBitcoin::ValueUpgraded::PriceByLevel qw(@price_by_level);
 
 use Exporter qw(import);
-our @EXPORT_OK = qw(level_by_total upgrade_value);
+our @EXPORT_OK = qw(level_by_total upgrade_value downgrade_value);
 
 use constant TABLE => 'value_upgraded';
 
@@ -42,6 +42,26 @@ sub upgrade_value {
     my ($value, $level) = @_;
 
     return int($value * $price_by_level[$level] / 1000000);
+}
+
+sub _downgrade_value_for_level {
+    my ($value, $level) = @_;
+
+    return int($value * 1000000 / $price_by_level[$level]);
+}
+
+sub downgrade_value {
+    my ($value, $upgraded) = @_;
+
+    my $level = level_by_total($upgraded);
+    my $btc_value = _downgrade_value_for_level($value, $level);
+    $btc_value = $upgraded if $btc_value > $upgraded;
+    while ($level > 0 && level_by_total($upgraded - $btc_value) < $level) {
+        $level--;
+        $btc_value = _downgrade_value_for_level($value, $level);
+        $btc_value = $upgraded if $btc_value > $upgraded;
+    }
+    return $btc_value;
 }
 
 1;
